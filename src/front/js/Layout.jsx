6cc7c82a94cc42ef6093cf722d246d1cf67cf15a
Javier Fuentes <jfuentescasta.m@gmail.com>
2025-02-07 15:54:51 +0000

    review de Javi

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import injectContext from "./store/appContext";
+// Custom component
 import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
-
+import { BackendURL } from "./component/BackendURL.jsx";
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/Footer.jsx";
+// Custom Pages for Views
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
-import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -28,7 +29,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<h1>Not found!</h1>} path="*" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
